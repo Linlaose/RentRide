af50fccde59dd367625773f667e55460cd469ab3
Linlaose <aa0900144903@gmail.com>
2023-03-02 17:27:14 +0800

    feat: promote video block completed

--- src/views/Index/IndexView.vue
+++ src/views/Index/IndexView.vue
@@ -3,6 +3,7 @@ import BannerComponent from '@/components/Banner/BannerComponent.vue';
 import NewsComponent from '@/components/News/NewsComponent.vue';
 import TopBikesComponent from '@/components/TopBikes/TopBikesComponent.vue';
 import SelectionComponent from '@/components/Selection/SelectionComponent.vue';
+import PromoteVideoComponent from '../../components/PromoteVideo/PromoteVideoComponent.vue';
 
 export default {
   name: 'IndexView',
@@ -11,6 +12,7 @@ export default {
     NewsComponent,
     TopBikesComponent,
     SelectionComponent,
+    PromoteVideoComponent,
   },
 };
 </script>
