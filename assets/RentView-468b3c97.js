d3b95e4b0e50c1d0c2cfb1ebff12a54099331826
Linlaose <aa0900144903@gmail.com>
2023-03-08 17:00:07 +0800

    deploy

--- assets/RentView-468b3c97.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,_ as n}from"./index-ff416110.js";function o(r,a,s,p,_,i){return e(),t("h1",null,"這是我要租車的頁面")}const c={name:"RentView"},f=n(c,[["render",o]]);export{f as default};
