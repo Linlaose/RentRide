f088c7525b5b2384ac1793bb393c39e2bd55e248
Linlaose <aa0900144903@gmail.com>
2023-03-21 21:23:52 +0800

    deploy

--- assets/ShopView-8dfc2ccd.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,_ as t}from"./index-fd72fc58.js";function n(r,a,s,p,_,i){return e(),o("h1",null,"這是重機商城的頁面")}const c={name:"ShopView"},f=t(c,[["render",n]]);export{f as default};
