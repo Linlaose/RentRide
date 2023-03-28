d8099de4d88f13d02b2324f3bd943c8e372b5d45
Linlaose <aa0900144903@gmail.com>
2023-03-29 07:27:11 +0800

    refactor: Switching topBikes data from local to remote storage

--- src/components/HomePage/TopBikes/TopBikesComponent.vue
+++ src/components/HomePage/TopBikes/TopBikesComponent.vue
@@ -11,25 +11,26 @@ export default {
   },
   data() {
     return {
-      top1: {
-        name: 'RS660',
-        horsePower: 100,
-        torque: 67,
-        displacement: 660,
-      },
-      top2: {
-        name: 'Ninja400',
-        horsePower: 45,
-        torque: 28,
-        displacement: 400,
-      },
-      top3: {
-        name: 'YZF-R6',
-        horsePower: 120,
-        torque: 62,
-        displacement: 600,
-      },
+      VITE_URL: import.meta.env.VITE_URL,
+      preTop1: {},
+      preTop2: {},
+      preTop3: {},
     };
+  },
+  computed: {
+    /* 
+    避免 `Avoid app logic that relies on enumerating keys on a component instance. The keys will be empty in production mode to avoid performance overhead.` 警告
+    所以 computed 產出畫面需要的渲染資料
+    */
+    top1() {
+      return { ...this.preTop1 };
+    },
+    top2() {
+      return { ...this.preTop2 };
+    },
+    top3() {
+      return { ...this.preTop3 };
+    },
   },
   methods: {
     scroller() {
@@ -148,6 +149,74 @@ export default {
         });
       });
     },
+    getTopBikes() {
+      const VITE_PATH = 'topBikes';
+      const url = `${this.VITE_URL}/${VITE_PATH}`;
+
+      this.$http
+        .get(url)
+        .then((res) => {
+          const { data } = res;
+          const [top1, top2, top3] = data;
+
+          this.callAll(top1.motorcycleId, top2.motorcycleId, top3.motorcycleId);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    callAll(top1Id, top2Id, top3Id) {
+      const VITE_PATH = 'motorcycles';
+
+      const getTop1 = this.$http.get(`${this.VITE_URL}/${VITE_PATH}/${top1Id}`);
+      const getTop2 = this.$http.get(`${this.VITE_URL}/${VITE_PATH}/${top2Id}`);
+      const getTop3 = this.$http.get(`${this.VITE_URL}/${VITE_PATH}/${top3Id}`);
+
+      Promise.all([getTop1, getTop2, getTop3])
+        .then((res) => {
+          // 取得三個回傳值的 data 屬性當作陣列
+          const data = res.map((item) => item.data);
+          // 將 data 的 item 解構賦值為新的物件變數
+          const [top1Data, top2Data, top3Data] = data;
+
+          /* 
+          把變數的特定屬性存入已定義的空物件中，這些物件不能直接作為渲染，
+          畫面會顯示不出資料，且有可能不跳錯誤，
+          唯一一次看到警告的訊息是，
+          `Avoid app logic that relies on enumerating keys on a component instance. The keys will be empty in production mode to avoid performance overhead.`
+          */
+          this.preTop1 = {
+            model: top1Data.model,
+            horsepower: Number(
+              Number(top1Data.horsepower.split('p')[0]).toFixed()
+            ),
+            torque:
+              Number(Number(top1Data.torque.split('k')[0]).toFixed(1)) * 10,
+            displacement: Number(top1Data.displacement),
+          };
+          this.preTop2 = {
+            model: top2Data.model,
+            horsepower: Number(
+              Number(top2Data.horsepower.split('p')[0]).toFixed()
+            ),
+            torque:
+              Number(Number(top2Data.torque.split('k')[0]).toFixed(1)) * 10,
+            displacement: Number(top2Data.displacement),
+          };
+          this.preTop3 = {
+            model: top3Data.model,
+            horsepower: Number(
+              Number(top3Data.horsepower.split('p')[0]).toFixed()
+            ),
+            torque:
+              Number(Number(top3Data.torque.split('k')[0]).toFixed(1)) * 10,
+            displacement: Number(top3Data.displacement),
+          };
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    },
   },
   mounted() {
     this.scroller();
@@ -155,6 +224,7 @@ export default {
       ScrollTrigger.refresh();
     }, 1500);
     window.addEventListener('scroll', this.getScrollHeight);
+    this.getTopBikes();
   },
 };
 </script>
